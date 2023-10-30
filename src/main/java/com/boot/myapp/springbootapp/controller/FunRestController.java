a943bc82b48f2eeebd4ccce735661acf2039a951
Student-Apna College <aditi.v@gmail.com>
2023-10-30 17:48:09 +0530

    adding entity

--- src/main/java/com/boot/myapp/springbootapp/controller/FunRestController.java
+++ src/main/java/com/boot/myapp/springbootapp/controller/FunRestController.java
@@ -1,15 +1,21 @@
 package com.boot.myapp.springbootapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
-    @GetMapping("/")
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teamInfo")
     public String myapp()
     {
-        return "Hello World";
+        return "Hello :"+coachName+" Welcome to the "+teamName;
     }
 
     @GetMapping("/hello")
